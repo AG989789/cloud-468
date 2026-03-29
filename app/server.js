74df5164cec6cce93be408567ac6cf2317e16686
AG989789 <126360569+AG989789@users.noreply.github.com>
2026-03-29 13:47:01 -0400

    Update server.js

--- app/server.js
+++ app/server.js
@@ -5,24 +5,19 @@ const path = require('path');
 
 const app = express();
 
-// Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// MongoDB connection (IMPORTANT: use "mongo")
 mongoose.connect('mongodb://mongo:27017/sharks', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
-// View engine setup
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Routes
 const indexRoutes = require('./routes/index');
 app.use('/', indexRoutes);
 
-// Start server
 app.listen(3000, () => {
   console.log('Server running on port 3000');
 });
