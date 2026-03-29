543589dbdfbc5aaa4cea840301941d95bbeb649b
AG989789 <126360569+AG989789@users.noreply.github.com>
2026-03-29 16:05:05 -0400

    Change node type to RawPC and update services

--- profile.py
+++ profile.py
@@ -1,16 +1,15 @@
 import geni.portal as portal
 import geni.rspec.pg as rspec
 
-# Create a Request object to start building the RSpec.
 request = portal.context.makeRequestRSpec()
-# Create a XenVM
-node = request.XenVM("node")
+
+node = request.RawPC("docker-node")
+
 node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD"
-node.routable_control_ip = "true"
 
-node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt update"))
-node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt install -y apache2"))
-node.addService(rspec.Execute(shell="/bin/sh", command='sudo systemctl status apache2'))
+node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt update && sudo apt install -y docker.io"))
+node.addService(rspec.Execute(shell="/bin/sh", command="sudo systemctl start docker"))
+node.addService(rspec.Execute(shell="/bin/sh", command="sudo systemctl enable docker"))
+node.addService(rspec.Execute(shell="/bin/sh", command="sudo usermod -aG docker ubuntu"))
 
-# Print the RSpec to the enclosing page.
 portal.context.printRequestRSpec()
