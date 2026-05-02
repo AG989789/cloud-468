0a5beceee77d73a8a598fd67583ff875c848ed28
AG989789 <126360569+AG989789@users.noreply.github.com>
2026-05-02 12:47:33 -0400

    Add JPA annotations to DeploymentRecord class
    Added JPA annotations for entity mapping and ID generation.

--- src/main/springprop/DeploymentRecord.java
+++ src/main/springprop/DeploymentRecord.java
@@ -1,6 +1,17 @@
 package springprop;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class DeploymentRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String applicationName;
     private String version;
     private String environment;
@@ -14,6 +25,14 @@ public class DeploymentRecord {
         this.version = version;
         this.environment = environment;
         this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getApplicationName() {
