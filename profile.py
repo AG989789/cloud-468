c1db0d33a4d0521a804e0240bfdd713c0287dda9
AG989789 <126360569+AG989789@users.noreply.github.com>
2026-05-02 14:32:39 -0400

    Update command to execute startup script

--- profile.py
+++ profile.py
@@ -8,7 +8,7 @@ node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD"
 
 node.addService(rspec.Execute(
     shell="sh",
-    command="echo profile-ran > /local/repository/profile-ran.txt"
+    command="cd /local/repository && chmod +x startup.sh && sh startup.sh > /local/repository/startup-run.log 2>&1"
 ))
 
 portal.context.printRequestRSpec()
