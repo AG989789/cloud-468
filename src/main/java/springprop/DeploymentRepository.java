11199fd41690326b274b8bb87eb40cd64051396e
AG989789 <126360569+AG989789@users.noreply.github.com>
2026-05-03 13:14:58 -0400

    Add method to find DeploymentRecord by name and environment

--- src/main/java/springprop/DeploymentRepository.java
+++ src/main/java/springprop/DeploymentRepository.java
@@ -2,5 +2,8 @@ package springprop;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface DeploymentRepository extends JpaRepository<DeploymentRecord, Long> {
+    Optional<DeploymentRecord> findByApplicationNameAndEnvironment(String applicationName, String environment);
 }
